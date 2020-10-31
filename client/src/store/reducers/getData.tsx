d0199392c3e62a0b2a976b3dab1403214a95f29a
skhan <skhan@staff.compass.edu.au>
2020-10-31 20:28:47 +1100

    reducer for getting data

---
+++ client/src/store/reducers/getData.tsx
@@ -0,0 +1,45 @@
+import { LOADING, ERROR, SEARCH_USERS, GET_USER_DETAILS } from "../types";
+
+interface IUserThin {
+  id: number;
+  name: string;
+  screen_name: string;
+}
+
+interface IUser {
+  id: number;
+  name: string;
+  screen_name: string;
+  profile_image_url_https: string;
+  followers_count: number;
+  last_five_tweets: string[];
+} 
+
+interface IState {
+  users: IUserThin[];
+  userDetails: IUser | null;
+  loading: boolean;
+  error: string | null;
+} 
+
+const initialState: IState = {
+  users: [],
+  userDetails: null,
+  loading: false,
+  error: null,
+}
+
+export default (state = initialState, action: any) => {
+  switch (action.type) {
+    case LOADING:
+      return { ...state, loading: action.payload }
+    case ERROR:
+      return { ...state, error: action.payload }
+    case SEARCH_USERS:
+      return { ...state, users: action.payload }
+    case GET_USER_DETAILS:
+      return { ...state, userDetails: action.payload }
+    default:
+      return state
+  }
+}
