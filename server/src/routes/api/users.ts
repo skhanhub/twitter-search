545c88fab2158314fea1743ec7f2f30c61fa912f
skhan <skhan@staff.compass.edu.au>
2020-10-31 11:34:15 +1100

    adding users routes

---
+++ server/src/routes/api/users.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const usersController = require("../../controllers/usersController");
+
+const ROUTER = express.Router();
+// Matches with "/api/users/search"
+ROUTER.route("/search")
+  .get(usersController.search)
+
+// Matches with "/api/users/show"
+ROUTER.route("/show")
+  .get(usersController.show)
+
+module.exports = ROUTER;
