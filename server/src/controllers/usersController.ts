d4323bbd8e73d99d7b5834bfab2105e4b0af2f72
skhan <skhan@staff.compass.edu.au>
2020-10-31 11:28:25 +1100

    adding show twites

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -2,9 +2,18 @@ import Twitter from 'twitter';
 import express from 'express'
  
 interface IUserThin {
-  id: string;
+  id: number;
   name: string;
-  screen_name: number;
+  screen_name: string;
+}
+
+interface IUser {
+  id: number;
+  name: string;
+  screen_name: string;
+  profile_image_url_https: string;
+  followers_count: number;
+  last_five_tweets: string[];
 }
 
 var client = new Twitter({
@@ -15,7 +24,7 @@ var client = new Twitter({
 });
 
 
-// Defining methods for the booksController
+// Defining methods for the userController
 module.exports = {
     search: function(req: express.Request, res: express.Response) {
 
@@ -25,7 +34,7 @@ module.exports = {
         count: req.query.count || 5,
       }
 
-      client.get('users/search.json', QUERY, function(error, users, response) {
+      client.get('users/search.json', QUERY, function(error, users) {
         if(error) throw new Error(error)
         
         const USER_LIST = users.map((user:any): IUserThin => ({
@@ -37,4 +46,36 @@ module.exports = {
         res.json(USER_LIST)
       });
     },
+    show: function(req: express.Request, res: express.Response) {
+
+      const QUERY = {
+        q: req.query.screen_name
+      }
+
+      client.get('users/show.json', QUERY, function(error, user) {
+        if(error) throw new Error(error)
+
+        const QUERY = {
+          screen_name: user.screen_name,
+          count: 5,
+        }
+        client.get('/statuses/user_timeline.json', QUERY, function(error: any, tweets: any) {
+          if(error) throw new Error(error)
+          const USER_DETAILS: IUser = {
+            id: user.id,
+            name: user.name,
+            screen_name: user.screen_name,
+            profile_image_url_https: user.profile_image_url_https,
+            followers_count: user.followers_count,
+            last_five_tweets: tweets.map((tweet: any)=>tweet.text),
+          }          
+          
+          console.log(USER_DETAILS);
+          res.json(USER_DETAILS)
+  
+        });
+
+      });
+    },
 };
+
