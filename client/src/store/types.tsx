bb783aaae985d59a51ab4babf553e148de26b055
skhan <skhan@staff.compass.edu.au>
2020-11-01 21:11:25 +1100

    ROWS_PER_PAGE SEARCH PAGE added

--- client/src/store/types.tsx
+++ client/src/store/types.tsx
@@ -1,5 +1,8 @@
 
-export const SEARCH_USERS = "SEARCH_USERS"
-export const GET_USER_DETAILS = "GET_USER_DETAILS"
-export const LOADING = "LOADING"
-export const ERROR = "ERROR"
+export const SEARCH_USERS = "SEARCH_USERS";
+export const GET_USER_DETAILS = "GET_USER_DETAILS";
+export const LOADING = "LOADING";
+export const ERROR = "ERROR";
+export const ROWS_PER_PAGE = "ROWS_PER_PAGE";
+export const SEARCH = "SEARCH";
+export const PAGE = "PAGE";
