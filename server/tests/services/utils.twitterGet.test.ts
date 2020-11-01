b03052d8f576d3f170c1c22490b8a9c848c231f9
skhan <skhan@staff.compass.edu.au>
2020-11-01 23:25:06 +1100

    test for twitterGet

---
+++ server/tests/services/utils.twitterGet.test.ts
@@ -0,0 +1,23 @@
+import twitterGet from "../../src/services/utils";
+
+jest.mock('twitter');
+
+describe("Tests for the twitterGet function in the utils service", () => {
+  test("Should return an objects with specific values", async () => {
+    //Arrange
+    // expect.assertions(3);
+    const correctResult = {
+        "name": "World Food Programme",
+        "id": 27830610,
+        "screen_name": "WFP"
+    }
+      
+    //Act
+    const result: any = await twitterGet("users/show.json", {screen_name: "WFP"});
+
+    //Assert
+    expect(result.name).toEqual(correctResult.name);
+    expect(result.screen_name).toEqual(correctResult.screen_name);
+    expect(result.id).toEqual(correctResult.id);
+  });
+});
