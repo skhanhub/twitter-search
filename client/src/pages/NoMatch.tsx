4c84df7c8347061e436a56d0311494220886787a
skhan <skhan@staff.compass.edu.au>
2020-11-01 21:09:22 +1100

    NoMatch Page init

---
+++ client/src/pages/NoMatch.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    textAlign: 'center'
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+function NoMatch() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <h1>404 Page Not Found</h1>
+        <h1>
+          <span role="img" aria-label="Face With Rolling Eyes Emoji">
+            🙄
+          </span>
+        </h1>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default NoMatch;
