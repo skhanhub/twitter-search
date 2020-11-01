81a0809ea3b9ef798900b6ee9d188d6288e2bfcd
skhan <skhan@staff.compass.edu.au>
2020-11-01 23:25:19 +1100

    clean up

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -8,10 +8,10 @@ const router = express.Router();
   GET route for fetching the index.html home page
   This route does not take any arguments
   It returns the index.html page
+  Need to build the client for this route to work
 */
 router.get('/', async (req, res, next) => {
   try{
-    console.log(path.join(__dirname, '../public'))
     res.status(200).sendFile(path.join(__dirname, '../public'));
   } catch(err) {
     // If there is an error then pass the error to the next function
