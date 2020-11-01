d9360c3f6fdd6f7c73446fd92ceb7c0312ab62f4
skhan <skhan@staff.compass.edu.au>
2020-11-01 21:10:54 +1100

    setPageNumber setSearchText setRowsPerPage added

--- client/src/store/actions/index.tsx
+++ client/src/store/actions/index.tsx
@@ -1,19 +1,21 @@
 import API from "../../utilities/API";
-import { LOADING, ERROR, SEARCH_USERS, GET_USER_DETAILS } from "../types";
+import { LOADING, ERROR, SEARCH_USERS, GET_USER_DETAILS, SEARCH, PAGE, ROWS_PER_PAGE } from "../types";
 
-export const searchUsers = (search: string) => async (dispatch: any) => {
+export const searchUsers = (search: string, page: number, rowsPerPage: number) => async (dispatch: any) => {
 
   try {
     dispatch({ type: LOADING, payload: true })
-    const DATA = await API.searchUsers(search);
-
-    dispatch({ type: SEARCH_USERS, payload: DATA })
+    if(search){
+      const DATA = await API.searchUsers(search, page, rowsPerPage);
+      dispatch({ type: SEARCH_USERS, payload: DATA })
+    }
     dispatch({ type: LOADING, payload: false })
   } catch (e) {
     dispatch({ type: ERROR, payload: e.message })
     dispatch({ type: LOADING, payload: false })
   }
 }
+
 
 
 export const getUserDetails = (id: number) => async (dispatch: any) => {
@@ -29,3 +31,9 @@ export const getUserDetails = (id: number) => async (dispatch: any) => {
     dispatch({ type: LOADING, payload: false })
   }
 }
+
+export const setPageNumber = (pageNumber: number) => (dispatch: any) =>  dispatch({ type: PAGE, payload: pageNumber })
+
+export const setSearchText = (search: string) => (dispatch: any) =>  dispatch({ type: SEARCH, payload: search })
+
+export const setRowsPerPage = (rowsPerPage: number) => (dispatch: any) =>  dispatch({ type: ROWS_PER_PAGE, payload: rowsPerPage })
