a6329b113734beb91a89df2feaaaac3e54345495
skhan <skhan@staff.compass.edu.au>
2020-11-01 21:50:51 +1100

    root route for serving index.html

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,23 @@
 import express from "express";
+import path from "path";
 import apiRoutes  from "./api";
 
 const ROUTER = express.Router();
 
+/*
+  GET route for fetching the index.html home page
+  This route does not take any arguments
+  It returns the index.html page
+*/
+ROUTER.get('/', async (req, res, next) => {
+  try{
+    console.log(path.join(__dirname, '../public'))
+    res.status(200).sendFile(path.join(__dirname, '../public'));
+  } catch(err) {
+    // If there is an error then pass the error to the next function
+    return next(err);
+  }
+})
 
 // API Routes
 ROUTER.use("/api", apiRoutes);
