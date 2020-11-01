76e4a48148f94f72e4a746e9ad3a3aa0fd64be25
skhan <skhan@staff.compass.edu.au>
2020-11-01 18:54:40 +1100

    creating a twitter service

---
+++ server/src/services/twitter.ts
@@ -0,0 +1,85 @@
+
+import twitterGet from "./utils";
+ 
+export interface ITweet{
+  tweet: string,
+  id: number,
+}
+interface IUserThin {
+  id: number;
+  name: string;
+  screen_name: string;
+}
+
+interface IUser {
+  id: number;
+  name: string;
+  screen_name: string;
+  profile_image_url_https: string;
+  followers_count: number;
+  last_five_tweets: ITweet[];
+}
+
+
+
+export default {
+  searchTwitter: async function(query: any, page: any = 1, count: any = 5) {
+    try{
+      const users: any = await twitterGet('users/search.json', {
+        q: query,
+        page,
+        count,
+      })
+      const USER_LIST = users.map((user:any): IUserThin => ({
+        screen_name: user.screen_name,
+        id: user.id,
+        name: user.name,
+      }))
+
+      return USER_LIST;
+    }
+    catch(err){
+      throw new Error(err);
+    }
+  },
+  getUserDetails: async function(id: any) {
+
+    try{
+      const user: any = await twitterGet(
+        'users/show.json', 
+        {
+          id
+        }
+      );
+
+      const TWEETS: any = await twitterGet(
+        'statuses/user_timeline.json', 
+        {
+          id: user.id,
+          count: 5,
+        }
+      );
+
+      const userDetails: IUser = {
+        id: user.id,
+        name: user.name,
+        screen_name: user.screen_name,
+        profile_image_url_https: user.profile_image_url_https,
+        followers_count: user.followers_count,
+        last_five_tweets: TWEETS.map((tweet: any)=>({
+          id: tweet.id,
+          tweet: tweet.text
+        })),
+      }                  
+
+      return userDetails;
+      
+    }
+    catch(err){
+      throw new Error(err);
+    }
+
+
+  },
+};
+
