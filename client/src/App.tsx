b21f9abc4c468b62ff420bc254174402b1273728
skhan <skhan@staff.compass.edu.au>
2020-11-01 21:06:27 +1100

    displaying errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
+import { useSelector } from "react-redux"
 import NoMatch from "./pages/NoMatch";
 import Home from "./pages/Home";
 import UserProfile from "./pages/UserProfile";
@@ -8,12 +9,17 @@ import UserProfile from "./pages/UserProfile";
 
 
 function App() {
+
+  const error = useSelector((state: any) => state.getData.error)
+
+
   return (
       <Router>
           <NavBar />
+          {error && <h3 style={{color: 'red', textAlign: 'center'}}>{error}</h3>}
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/UserProfile" component={UserProfile} />
+            <Route exact path="/UserProfile/:id" component={UserProfile} />
             <Route component={NoMatch} />
           </Switch>
       </Router>
