954a030369444946add0f33d403c6f92d2a7399b
skhan <skhan@staff.compass.edu.au>
2020-11-01 18:56:40 +1100

    adding rowsPerpage nad page options

--- client/src/utilities/API.ts
+++ client/src/utilities/API.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export default {
-    searchUsers: async function(search: string){
-        const result = await axios.get(`/api/users/search?q=${search}`);
+    searchUsers: async function(search: string, page: number, rowsPerPage: number){
+        const result = await axios.get(`/api/users/search?q=${search}&count=${rowsPerPage}&page=${page}`);
         return result.data;
     },
     getUserDetails: async function(id: number){
-      const result = await axios.get(`/api/users/search?q=${id}`);
+      const result = await axios.get(`/api/users/show?q=${id}`);
       return result.data;
   }
 }
