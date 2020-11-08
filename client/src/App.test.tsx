06ba9a36b24fafdb61807ef9fb56b74007a91d1f
skhan <skhan@staff.compass.edu.au>
2020-11-08 21:51:11 +1100

    simplifying test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { shallow } from "enzyme";
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
 import thunk from "redux-thunk";
@@ -21,8 +20,7 @@ describe("Tests for the App component", () => {
 
   it("Should have the text Twitter Search", () => {
     store = buildStore(initialState);
-    render(<Provider store={store}><App /></Provider>)
-    const linkElement = screen.getByText(/Twitter Search/i);
-    expect(linkElement).toBeInTheDocument();
+    const {queryByText} = render(<Provider store={store}><App /></Provider>)
+    expect(queryByText(/Twitter Search/i)).toBeInTheDocument();
   });
 });
