01e1d578444344d61ec6a3de88e18c11a5703ff6
hasankhs <shahriar.hasankhan@anz.com>
2021-07-03 18:07:12 +1000

    use twitterClient

--- server/src/services/twitter.ts
+++ server/src/services/twitter.ts
@@ -1,5 +1,5 @@
 
-import twitterGet from "./utils";
+import twitterClient from "./twitter.client";
  
 export interface ITweet{
   tweet: string,
@@ -24,62 +24,49 @@ interface IUser {
 
 export default {
   searchTwitter: async function(query: string, page: number = 1, count: number = 5) {
-    try{
-      const users: any = await twitterGet("users/search.json", {
-        q: query,
-        page,
-        count,
-      })
-      const userList = users.map((user: any): IUserThin => ({
-        screen_name: user.screen_name,
-        id: user.id,
-        name: user.name,
-      }))
+    
+    const users: any = await twitterClient.get("users/search.json", {
+      q: query,
+      page,
+      count,
+    })
+    const userList = users.map((user: any): IUserThin => ({
+      screen_name: user.screen_name,
+      id: user.id,
+      name: user.name,
+    }))
 
-      return userList;
-    }
-    catch(err){
-      throw new Error(err);
-    }
+    return userList;
   },
   getUserDetails: async function(screen_name: string) {
-
-    try{
       
-      const user: any = await twitterGet(
-        "users/show.json", 
-        {
-          screen_name
-        }
-      );
-      const tweets: any = await twitterGet(
-        "statuses/user_timeline.json", 
-        {
-          id: user.screen_name,
-          count: 5,
-        }
-      );
+    const user: any = await twitterClient.get(
+      "users/show.json", 
+      {
+        screen_name
+      }
+    );
+    const tweets: any = await twitterClient.get(
+      "statuses/user_timeline.json", 
+      {
+        id: user.screen_name,
+        count: 5,
+      }
+    );
 
-      const userDetails: IUser = {
-        id: user.id,
-        name: user.name,
-        screen_name: user.screen_name,
-        profile_image_url_https: user.profile_image_url_https,
-        followers_count: user.followers_count,
-        last_five_tweets: tweets.map((tweet: any): ITweet=>({
-          id: tweet.id,
-          tweet: tweet.text
-        })),
-      }                  
+    const userDetails: IUser = {
+      id: user.id,
+      name: user.name,
+      screen_name: user.screen_name,
+      profile_image_url_https: user.profile_image_url_https,
+      followers_count: user.followers_count,
+      last_five_tweets: tweets.map((tweet: any): ITweet=>({
+        id: tweet.id,
+        tweet: tweet.text
+      })),
+    }                  
 
-      return userDetails;
-      
-    }
-    catch(err){
-      throw new Error(err);
-    }
-
-
+    return userDetails;
   },
 };
 
