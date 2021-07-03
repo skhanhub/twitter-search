988e8b051eb69d6f6e55c78b8750504dafa08b45
hasankhs <shahriar.hasankhan@anz.com>
2021-07-03 18:40:36 +1000

    detect missing params

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -1,26 +1,41 @@
 import { Request, Response } from 'express'
 import { StatusCodes, getReasonPhrase } from 'http-status-codes';
+import { MISSING_PARAMS } from "../constant/error.constant";
 import twitter from "../services/twitter";
 
 // Defining methods for the userController
 export default {
     search: async function(req: Request, res: Response) {
       try{
-        const userList = await twitter.searchTwitter(req.query.q as string, Number(req.query.page), Number(req.query.count))
-        res.status(StatusCodes.OK).json(userList)
+        if (req.query.q){
+          const userList = await twitter.searchTwitter(req.query.q as string, Number(req.query.page), Number(req.query.count))
+          res.status(StatusCodes.OK).json(userList)
+        } else {
+          return res.status(StatusCodes.BAD_REQUEST).json(MISSING_PARAMS)
+        }
       }
       catch(err){
         console.error(err)
+        if(Array.isArray(err) && err[0].message) {
+          return res.status(StatusCodes.BAD_REQUEST).json(err[0].message)
+        }
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR))
       }
     },
     show: async function(req: Request, res: Response) {
       try{
-        const userDetails = await twitter.getUserDetails(req.query.q as string); 
-        res.status(StatusCodes.OK).json(userDetails)
+        if (req.query.q){
+          const userDetails = await twitter.getUserDetails(req.query.q as string); 
+          res.status(StatusCodes.OK).json(userDetails)
+        } else {
+          return res.status(StatusCodes.BAD_REQUEST).json(MISSING_PARAMS)
+        }
       }
       catch(err){
         console.error(err)
+        if(Array.isArray(err) && err[0].message) {
+          return res.status(StatusCodes.BAD_REQUEST).json(err[0].message)
+        }
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR))
       }
     },
