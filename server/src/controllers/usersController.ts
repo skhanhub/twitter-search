9a26397ec1b6cf695a64d68ee23d74fc0f82c12c
hasankhs <shahriar.hasankhan@anz.com>
2021-07-03 13:16:27 +1000

    use http-status-codes

--- server/src/controllers/usersController.ts
+++ server/src/controllers/usersController.ts
@@ -1,35 +1,28 @@
-import express from 'express'
+import { Request, Response } from 'express'
+import { StatusCodes, getReasonPhrase } from 'http-status-codes';
 import twitter from "../services/twitter";
-
-
-
 
 // Defining methods for the userController
 export default {
-    search: async function(req: express.Request, res: express.Response) {
-
+    search: async function(req: Request, res: Response) {
       try{
         const userList = await twitter.searchTwitter(req.query.q as string, Number(req.query.page), Number(req.query.count))
-        res.json(userList)
+        res.status(StatusCodes.OK).json(userList)
       }
       catch(err){
-        console.log(err)
-        res.status(422).json(err)
+        console.error(err)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR))
       }
     },
-    show: async function(req: express.Request, res: express.Response) {
-
+    show: async function(req: Request, res: Response) {
       try{
         const userDetails = await twitter.getUserDetails(req.query.q as string); 
-        res.json(userDetails)
-        
+        res.status(StatusCodes.OK).json(userDetails)
       }
       catch(err){
-        console.log(err)
-        res.status(422).json(err)
+        console.error(err)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR))
       }
-
-
     },
 };
 
